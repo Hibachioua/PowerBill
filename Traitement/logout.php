07333b74dc59ce5561004713768123d7277df30a
hichioua <hiba.chioua.st@emeal.nttdata.com>
2025-03-24 16:19:47 +0000

    fix logout

--- Traitement/logout.php
+++ Traitement/logout.php
@@ -1,22 +1,14 @@
 <?php
-// Démarrer la session
-session_start();
+// logout.php - À placer dans le dossier Traitement
+require_once "../BD/connexion.php";
+require_once "../BD/LoginController.php";
 
+// Créer l'instance du contrôleur
+$db = DB::connect();
+$loginController = new LoginController($db);
 
-$_SESSION = array();
-
-// Détruire le cookie de session
-if (isset($_COOKIE[session_name()])) {
-    setcookie(session_name(), '', time()-42000, '/');
-}
-
-// Détruire la session
-session_destroy();
-
-// Supprimer le cookie "remember_user" s'il existe
-if (isset($_COOKIE['remember_user'])) {
-    setcookie('remember_user', '', time() - 3600, '/');
-}
+// Appeler la méthode de déconnexion du contrôleur
+$logoutSuccess = $loginController->logout();
 
 // Rediriger vers la page d'accueil
 header("Location: ../IHM/index.php");
