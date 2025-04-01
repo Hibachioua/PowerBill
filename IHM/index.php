7f3e0e3fde841271ae87d26990a8a50d00944d6c
hichioua <hiba.chioua.st@emeal.nttdata.com>
2025-04-01 16:11:05 +0000

    fix services section

--- IHM/index.php
+++ IHM/index.php
@@ -7,7 +7,7 @@
     <title>PowerBill - login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>   
 <!-- Hero Section -->
 <section class="hero">
@@ -51,7 +51,7 @@
                 <p>Obtenez une assistance instantanée pour tout problème lié à la facturation.</p>
             </div>
             <div class="col-md-4">
-                <img src="assets/images/meter.svg" alt="Automated Readings" class="img-fluid">
+                <img src="assets/images/meterr.svg" alt="Automated Readings" class="img-fluid">
                 <h4>Relevés de Compteur Automatisés</h4>
                 <p>Recevez des mises à jour précises et en temps réel sur votre consommation d'électricité.</p>
             </div>
@@ -245,7 +245,7 @@
 </section>
 
 <!-- JavaScript pour le défilement doux vers les ancres -->
-<script src="assets/js/index.js"></script>
+<script src="assets/js/script_index.js"></script>
 
 
 <?php include "footer.php"; ?>
